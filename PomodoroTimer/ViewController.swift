2f14c35b3f01b04e15f652356dfc83e503bb8e20
miharu.matsumoto <matsumoto_miharu@plise.co.jp>
2019-02-23 00:18:08 +0900

    install pod and create menu

--- PomodoroTimer/ViewController.swift
+++ PomodoroTimer/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    @IBOutlet weak var miniteLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var todoText: UITextField!
+    private var timer = Timer()
+    private var count = 30 * 60
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func startTimer(_ sender: Any) {
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+        if self.startButton.currentTitle == "Start" {
+            startButton.setTitleColor(UIColor(red: 0, green: 122 / 255, blue: 1, alpha: 1), for: .normal)
+            startButton.setTitle("Stop", for: .normal)
+            time.text = "集中時間"
+            time.backgroundColor = UIColor(red: 0, green: 122 / 255, blue: 1, alpha: 1)
+        } else if self.startButton.currentTitle == "Stop" {
+            timer.invalidate()
+            count = 30 * 60
+            miniteLabel.text = String(30)
+            secondLabel.text = "0" + String(0)
+            startButton.setTitleColor(UIColor(red: 0, green: 122 / 255, blue: 1, alpha: 1), for: .normal)
+            startButton.setTitle("Start", for: .normal)
+        }
+    }
+    
+    @objc private func updateTimer() {
+        if count == 0 {
+            
+            timer.invalidate()
+            count = 30 * 60
+            startButton.setTitleColor(UIColor(red: 0, green: 122 / 255, blue: 1, alpha: 1), for: .normal)
+            startButton.setTitle("Start", for: .normal)
+        } else if count == 5 * 60 {
+            time.text = "休憩時間"
+            time.backgroundColor = UIColor(red: 199 / 255, green: 1, blue: 136 / 255, alpha: 1)
+            count -= 1
+        } else {
+            count -= 1
+        }
+        var miniteCount = count / 60
+        var secondCount = count % 60
+        
+        if miniteCount < 10 {
+            miniteLabel.text = "0" + String(miniteCount)
+        } else {
+            miniteLabel.text = String(miniteCount)
+        }
+        if secondCount < 10 {
+            secondLabel.text = "0" + String(secondCount)
+        } else {
+            secondLabel.text = String(secondCount)
+        }
+    }
 }
 
